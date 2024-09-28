efc3447d0ae44b5cca1ee6dd43feb5a952bdb614
Krushit-Babariya <krushitsbabariya41@gmial.com>
2024-09-28 16:47:59 +0530

    initial commit

---
+++ Splitmoney/src/main/java/com/krushit/SplitmoneyApplication.java
@@ -0,0 +1,13 @@
+package com.krushit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitmoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitmoneyApplication.class, args);
+	}
+
+}
